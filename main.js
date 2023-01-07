6daf1585a1e6e3d68dd08810f1e6de0e8116083f
Couprie <jarod64@hotmail.fr>
2023-01-07 19:47:06 +0100

    final colors

--- main.js
+++ main.js
@@ -12,15 +12,15 @@ let word = newWord();
 let alreadyTriedKeys = [];
 let count = 0;
 
-const letterDisplayedContainer = document.querySelector("#tusmo-container");
-const letterDisplayed = letterDisplayedContainer.querySelectorAll(".tusmo-item");
+const letterDisplayedContainer = document.querySelector("#letterDisplayedContainer");
+const letterDisplayed = letterDisplayedContainer.querySelectorAll(".letterDisplayed");
 
-const keyboardKey = document.querySelector("#key-container");
-const keyLetter = keyboardKey.querySelectorAll(".key");
+const keysContainer = document.querySelector("#key-container");
+const kbKey = keysContainer.querySelectorAll(".key");
 
-function letterInKeyboard(nodeListKey, hasClassName, toAddClassName, keyTried){
+function letterInKeyboard(nodeListKey, toAddClassName, keyTried){
     for (let i = 0; i < nodeListKey.length; i++){
-        if (nodeListKey[i].className === hasClassName+keyTried){
+        if (nodeListKey[i].innerText.toLowerCase() === keyTried){
             nodeListKey[i].classList.add(toAddClassName);
         };
     };
@@ -37,13 +37,14 @@ function isKeyInWord(keyTried){
             for (let i = 0; i < letterDisplayed.length; i++){
                 if (keyTried === word[i]){
                     letterDisplayed[i].innerText = word[i];
+                    letterDisplayed[i].classList.add("right-key");
                     count++;
                 };
             };
             alreadyTriedKeys.push(keyTried);
-            letterInKeyboard(keyLetter, "key ", "right-key", keyTried);
+            letterInKeyboard(kbKey, "right-key", keyTried);
         }else{
-            letterInKeyboard(keyLetter, "key ", "wrong-key", keyTried);
+            letterInKeyboard(kbKey, "wrong-key", keyTried);
             alreadyTriedKeys.push(keyTried);
         };
         if (count >= 7){
@@ -52,7 +53,7 @@ function isKeyInWord(keyTried){
     };
 };
 
-keyLetter.forEach(key => key.addEventListener("click", (event) =>{
+kbKey.forEach(key => key.addEventListener("click", (event) =>{
     let keyTried = event.currentTarget.innerText.toLowerCase();
     isKeyInWord(keyTried);
 }));
@@ -61,7 +62,7 @@ document.querySelector("form").addEventListener("submit", (event) =>{
     event.preventDefault();
     const form = event.currentTarget;
     const data = new FormData(form);
-    let keyTried = data.get("letter-try").trim().toLowerCase();
+    let keyTried = data.get("key-try").trim().toLowerCase();
     form.reset();
     isKeyInWord(keyTried);
 });
@@ -77,10 +78,11 @@ document.querySelector("#new-word").addEventListener("click", () =>{
     for (let i = 0; i < letterDisplayed.length; i++){
         if(letterDisplayed[i].innerText !== ""){
             letterDisplayed[i].innerText = "";
+            removeClass(letterDisplayed[i], "right-key");
         };
     };
-    for (let i = 0; i < keyLetter.length; i++){
-        removeClass(keyLetter[i],"wrong-key");
-        removeClass(keyLetter[i], "right-key");
+    for (let i = 0; i < kbKey.length; i++){
+        removeClass(kbKey[i],"wrong-key");
+        removeClass(kbKey[i], "right-key");
     };
 });
