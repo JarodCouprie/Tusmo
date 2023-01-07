fb6558e14e6cea5fe1dc93ce25000e21f39743b6
Couprie <jarod64@hotmail.fr>
2023-01-07 16:15:36 +0100

    clickable keyboard on screen

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-let alphabet = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"];
+const alphabet = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"];
 
 function newWord(){
     let randomTab = [];
@@ -8,6 +8,9 @@ function newWord(){
     return randomTab;
 }
 
+let word = newWord();
+let alreadyTriedKeys = [];
+let count = 0;
 
 const letterDisplayedContainer = document.querySelector("#tusmo-container");
 const letterDisplayed = letterDisplayedContainer.querySelectorAll(".tusmo-item");
@@ -15,11 +18,53 @@ const letterDisplayed = letterDisplayedContainer.querySelectorAll(".tusmo-item")
 const keyboardKey = document.querySelector("#key-container");
 const keyLetter = keyboardKey.querySelectorAll(".key");
 
-let word = newWord();
-let alreadyTriedKeys = [];
-console.log(word);
+function letterInKeyboard(nodeListKey, hasClassName, toAddClassName, keyTried){
+    for (let i = 0; i < nodeListKey.length; i++){
+        if (nodeListKey[i].className === hasClassName+keyTried){
+            nodeListKey[i].classList.add(toAddClassName);
+        };
+    };
+};
 
-let count = 0;
+function isKeyInWord(keyTried){
+    if(!alphabet.includes(keyTried)){
+        window.alert("You must use one letter from the latin alphabet");
+    };
+    if (alreadyTriedKeys.includes(keyTried)){
+        return;
+    }else{
+        if (word.includes(keyTried)){
+            for (let i = 0; i < letterDisplayed.length; i++){
+                if (keyTried === word[i]){
+                    letterDisplayed[i].innerText = word[i];
+                    count++;
+                };
+            };
+            alreadyTriedKeys.push(keyTried);
+            letterInKeyboard(keyLetter, "key ", "right-key", keyTried);
+        }else{
+            letterInKeyboard(keyLetter, "key ", "wrong-key", keyTried);
+            alreadyTriedKeys.push(keyTried);
+        };
+        if (count >= 7){
+            console.log("CONGRATULATIONS ! You found the word");
+        };
+    };
+};
+
+keyLetter.forEach(key => key.addEventListener("click", (event) =>{
+    let keyTried = event.currentTarget.innerText.toLowerCase();
+    isKeyInWord(keyTried);
+}));
+
+document.querySelector("form").addEventListener("submit", (event) =>{
+    event.preventDefault();
+    const form = event.currentTarget;
+    const data = new FormData(form);
+    let keyTried = data.get("letter-try").trim().toLowerCase();
+    form.reset();
+    isKeyInWord(keyTried);
+});
 
 function removeClass(element, classToRemove){
     element.classList.remove(classToRemove);
@@ -39,50 +84,3 @@ document.querySelector("#new-word").addEventListener("click", () =>{
         removeClass(keyLetter[i], "right-key");
     };
 });
-
-
-document.querySelector("form").addEventListener("submit", (event) =>{
-    event.preventDefault();
-    const form = event.currentTarget;
-    const data = new FormData(form);
-    let keyTried = data.get("letter-try").trim().toLowerCase();
-    if (keyTried.length > 1){
-        return;
-    }
-    form.reset();
-
-    function letterInKeyboard(nodeListKey, hasClassName, toAddClassName){
-        for (let i = 0; i < nodeListKey.length; i++){
-            if (nodeListKey[i].className === hasClassName+keyTried){
-                nodeListKey[i].classList.add(toAddClassName);
-            };
-        };
-    }
-    if (alreadyTriedKeys.includes(keyTried)){
-        console.log(keyTried+" has alredy been used")
-    }else{
-        if (word.includes(keyTried)){
-            for (let i = 0; i < letterDisplayed.length; i++){
-                if (keyTried === word[i]){
-                    letterDisplayed[i].innerText = word[i];
-                    count++;
-                };
-            };
-            alreadyTriedKeys.push(keyTried);
-            letterInKeyboard(keyLetter, "key ", "right-key");
-        }else{
-            letterInKeyboard(keyLetter, "key ", "wrong-key");
-            alreadyTriedKeys.push(keyTried);
-        };
-    };
-    console.log(alreadyTriedKeys);
-    if (count >= 7){
-        console.log("WIN");
-    };
-});
-
-
-
-
-
-
